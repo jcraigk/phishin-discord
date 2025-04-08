bae0e541df7171a8f2437b19e958c5a679a540fd
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2025-04-08 14:07:38 -0700

    Fix

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ client.once("ready", async () => {
     console.error(error);
   }
 });
-dokku checks:disable phishin-discord worker
 
 // Handle new guild joins
 client.on("guildCreate", async (guild) => {
